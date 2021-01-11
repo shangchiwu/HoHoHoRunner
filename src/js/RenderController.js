fae53197dfb059a1eec32928cdea30e2ed9aa1eb
Shang Chi Wu <shangchi537@gmail.com>
2021-01-11 22:41:29 +0800

    Fix: fix direction with zero degree to right

--- src/js/RenderController.js
+++ src/js/RenderController.js
@@ -71,7 +71,7 @@ class RenderController {
    */
   setCamera([x, y], direction) {
     // change coordinate system
-    direction = (direction + 270) % 360;
+    direction = (-1 * direction + 270) % 360;
 
     // move camera
     this.camera.position.x = x;
